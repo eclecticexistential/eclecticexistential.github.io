47672516ca6828d127f80a00e97ba034dba6c724
eclecticexistential <allofitbra@gmail.com>
2018-04-23 01:22:05 -0700

    New Python page added for dice game.

---
+++ python/dice.js
@@ -0,0 +1,67 @@
+(function() {
+            var fn = function() {
+              Bokeh.safely(function() {
+                (function(root) {
+                  function embed_document(root) {
+                    
+                  var docs_json = document.getElementById('3f34951b-fd14-44cd-b316-60285cc620a7').textContent;
+                  var render_items = [{"docid":"15be6c81-c43a-447e-939b-f60672e9dfdc","elementid":"762f21ae-8699-42dc-afdd-7c27e63c3dc5","modelid":"df085b19-d823-43bf-ad7d-13568f34cdfa"}];
+                  root.Bokeh.embed.embed_items(docs_json, render_items);
+                
+                  }
+                  if (root.Bokeh !== undefined) {
+                    embed_document(root);
+                  } else {
+                    var attempts = 0;
+                    var timer = setInterval(function(root) {
+                      if (root.Bokeh !== undefined) {
+                        embed_document(root);
+                        clearInterval(timer);
+                      }
+                      attempts++;
+                      if (attempts > 100) {
+                        console.log("Bokeh: ERROR: Unable to run BokehJS code because BokehJS library is missing")
+                        clearInterval(timer);
+                      }
+                    }, 10, root)
+                  }
+                })(window);
+              });
+            };
+            if (document.readyState != "loading") fn();
+            else document.addEventListener("DOMContentLoaded", fn);
+          })();
+		  
+(function() {
+            var fn = function() {
+              Bokeh.safely(function() {
+                (function(root) {
+                  function embed_document(root) {
+                    
+                  var docs_json = document.getElementById('42614eef-7ff7-4584-b194-1f2d84f06401').textContent;
+                  var render_items = [{"docid":"6b662ff7-b9a5-41bc-90e6-ffa982497c1b","elementid":"05a6fd0b-1b28-4b61-a044-8e23be84ddbe","modelid":"ab70e05b-6691-4947-87d3-597b89decade"}];
+                  root.Bokeh.embed.embed_items(docs_json, render_items);
+                
+                  }
+                  if (root.Bokeh !== undefined) {
+                    embed_document(root);
+                  } else {
+                    var attempts = 0;
+                    var timer = setInterval(function(root) {
+                      if (root.Bokeh !== undefined) {
+                        embed_document(root);
+                        clearInterval(timer);
+                      }
+                      attempts++;
+                      if (attempts > 100) {
+                        console.log("Bokeh: ERROR: Unable to run BokehJS code because BokehJS library is missing")
+                        clearInterval(timer);
+                      }
+                    }, 10, root)
+                  }
+                })(window);
+              });
+            };
+            if (document.readyState != "loading") fn();
+            else document.addEventListener("DOMContentLoaded", fn);
+          })();
